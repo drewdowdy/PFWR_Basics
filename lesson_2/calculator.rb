65b0b8ab4a9c747e9e8eb48b4976c45d0157004f
Drew D <drewdowdy109@gmail.com>
2023-08-07 16:03:14 -0400

    updated based on rubocop

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -13,14 +13,14 @@ end
 
 def operation_to_message(op)
   case op
-  when '1'
-    "Adding"
-  when '2'
-    "Subtracting"
-  when '3'
-    "Multiplying"
-  when '4'
-    "Dividing"
+    when '1'
+      "Adding"
+    when '2'
+      "Subtracting"
+   when '3'
+      "Multiplying"
+    when '4'
+     "Dividing"
   end
 end
 
@@ -88,7 +88,7 @@ loop do #main loop
     when '1'
       number1.to_i() + number2.to_i()
     when '2'
-      number1.to_i() - number2.to_i() 
+      number1.to_i() - number2.to_i()
     when '3'
       number1.to_i() * number2.to_i()
     when '4'
