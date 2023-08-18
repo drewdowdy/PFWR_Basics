c24bf997b98b54bcbbd84bc8d208a802af927633
Drew D <drewdowdy109@gmail.com>
2023-08-18 16:39:51 -0400

    created q5.rb

---
+++ lesson3/easy2/q5.rb
@@ -0,0 +1,4 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+p easy_flinstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+  # %w creates an array of strings divided by spaces
