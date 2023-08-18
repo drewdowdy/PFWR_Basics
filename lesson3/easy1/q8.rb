7f1e96c207805f87c14c163c953365e83e8a05fb
Drew D <drewdowdy109@gmail.com>
2023-08-18 15:59:50 -0400

    updated q8.rb

--- lesson3/easy1/q8.rb
+++ lesson3/easy1/q8.rb
@@ -1,6 +1,8 @@
 flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
 
 p (flintstones.select{|k,v| k == "Barney"}).flatten
+  # `Hash#select{|key,value|...}` returns a new Hash object whose entries are those for which the block returns a truthy value
+  # `Hash#flatten` returns a new Array object that is a 1-dimensional flattening of self
 
 p flintstones.assoc("Barney") 
-  # Hash#assoc if the given key is found, returns a 2-element Array containing that key and its value
+  # `Hash#assoc` if the given key is found, returns a 2-element Array containing that key and its value
