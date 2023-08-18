755da885c63ee2bbefc9696abf21eeb6b239f8bf
Drew D <drewdowdy109@gmail.com>
2023-08-18 14:32:06 -0400

    created q5.rb

---
+++ lesson3/easy1/q5.rb
@@ -0,0 +1,5 @@
+p (10..100).include?(42) 
+  # (array) returns true if for some index i in self, obj == self[i]; otherwise false
+
+p (10..100).cover?(42) 
+  # (range) returns true if the given argument is within self, false otherwise
