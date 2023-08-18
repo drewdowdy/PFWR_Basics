d95a74b7c4566f4a7e182588f157510e4eeb0c1a
Drew D <drewdowdy109@gmail.com>
2023-08-18 13:24:25 -0400

    updated q1.rb

---
+++ lesson3/easy1/q1.rb
@@ -0,0 +1,8 @@
+numbers = [1, 2, 2, 3]
+numbers.uniq # .uniq returns a new array w/out duplicates (NOT MUTABLE W/OUT !)
+
+puts numbers # puts on an array prints each element on a new line
+# => 1
+# => 2
+# => 2
+# => 3
